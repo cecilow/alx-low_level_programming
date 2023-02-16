b08e842322c605162ed4b486eb0537aa3455dae5
Cecil Wiafe <cecilwiafe@gmail.com>
2023-02-16 07:44:20 -0800

    print various data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - function description
+ * 
+ * Description: print sizes of various types 
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	char charType;
+	int IntegerType;
+	long int x;
+	long long int y;
+	float FloatType;
+
+	printf("Size of a char: %zu byte(s)\n",sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n",sizeof(IntegerType));
+	printf("Size of a long int: %zu byte(s)\n",sizeof(x));
+	printf("Size of a long long int: %zu byte(s)\n",sizeof(y));
+	printf("Size of a float: %zu byte(s)\n",sizeof(FloatType));
+	return (0);
+}
