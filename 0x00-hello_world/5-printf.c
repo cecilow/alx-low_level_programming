7ef607c8c6452c646f0cbd4a1f2930fc66b18609
Cecil Wiafe <cecilwiafe@gmail.com>
2023-02-16 07:15:32 -0800

    edit hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main(void){
+/**
+ * main - this is a descriptin
+ */
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
