741d5caa7ec74b37d97e194711675063185e1583
Cecil Wiafe <cecilwiafe@gmail.com>
2023-02-17 13:48:30 -0800

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main funcion
+ * Return: return zero
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
