0f9e0968661dbac05fb85d184fc77476c36204ef
Cecil Wiafe <cecilwiafe@gmail.com>
2023-02-23 06:36:48 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: return zero
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
