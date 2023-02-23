41757b227a65f5ca5f7fc044bd2cb6f3b8670c52
Cecil Wiafe <cecilwiafe@gmail.com>
2023-02-23 11:31:13 -0800

    multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+void times_table(void);
+void print_to_98(int n);
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-
 #endif
