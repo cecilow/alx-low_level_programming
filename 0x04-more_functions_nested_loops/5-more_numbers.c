7d51c14646cc1f6b3c8c8d8923825accfcc180a1
Cecil Wiafe <cecilwiafe@gmail.com>
2023-02-23 09:06:17 -0800

    print more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,13 @@ void more_numbers(void)
 	int j;
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
 			{
 				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
 			}
 			_putchar(j % 10 + '0');
 		}
