65a4644578cae466e7c6aaf965311b8a1423457e
Cecil Wiafe <cecilwiafe@gmail.com>
2023-02-23 09:10:44 -0800

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,12 +11,14 @@ void print_diagonal(int n)
 	int j;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
 			}
