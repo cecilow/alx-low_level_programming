87c07b915df60d255899f9cefb2920c326e11275
Cecil Wiafe <cecilwiafe@gmail.com>
2023-02-23 11:43:52 -0800

    multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,6 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+#include <stdio.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
