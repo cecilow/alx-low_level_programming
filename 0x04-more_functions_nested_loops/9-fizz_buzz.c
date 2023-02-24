686af911a4fc1849dcc7cb5c939c736c0ca2ab1d
Cecil Wiafe <cecilwiafe@gmail.com>
2023-02-23 16:53:54 -0800

    print fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 5 == 0 && i % 3 == 0)
 		{
@@ -28,7 +28,7 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
-	printf("\n");
+	printf("Buzz\n");
 
 	return (0);
 }
