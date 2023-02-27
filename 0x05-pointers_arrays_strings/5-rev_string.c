45a109ace65f1d2fcbecc5085c78b8bd12369cae
Cecil Wiafe <cecilwiafe@gmail.com>
2023-02-27 06:59:43 -0800

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - reverse a sting
+ * @s: the string to be rev
+ *
+ * Return: return zero
+ */
+void rev_string(char *s)
+{
+	int i;
+	char l;
+
+	for (i = 0; i < _strlen(s) / 2; i++)
+	{
+		l = s[i];
+		s[i] = s[_strlen(s) - 1 - i];
+		s[_strlen(s) - 1 - i] = l;
+	}
+}
