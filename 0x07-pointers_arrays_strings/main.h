22384d38dc79083f3766361d55edd2f98a18e613
Cecil Wiafe <cecilwiafe@gmail.com>
2023-03-06 10:56:18 -0800

    main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -48,4 +48,8 @@ char *leet(char *s);
 char *rot13(char *);
 void print_number(int n);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 #endif
