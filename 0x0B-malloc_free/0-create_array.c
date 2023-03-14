a6215129da787fd33017e8b84ce62d507ad9f1f2
Cecil Wiafe <cecilwiafe@gmail.com>
2023-03-13 19:27:53 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,13 +19,14 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	ptr = (char *) malloc(size * sizeof(char));
-	if (ptr != NULL)
+	if (ptr == NULL)
 	{
-		for (i = 0; i < size; i++)
-		{
-			ptr[i] = c;
-		}
+		return (NULL);
 	}
-	return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+
 	return (ptr);
 }
