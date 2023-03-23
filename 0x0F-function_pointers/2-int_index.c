3b74118bae8f8c1f650d09f1d9a590983acd55c3
Cecil Wiafe <cecilwiafe@gmail.com>
2023-03-22 19:32:12 -0700

    search for integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,18 +14,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	int index;
 
-	if (array != NULL || cmp != NULL)
-	{
-		if (size <= 0)
-			return (-1);
-		for (i = 0; i < size; i++)
-		{
-			index = cmp(array[i]);
-			if (index)
-			{
-				return (i);
-			}
-		}
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		index = cmp(array[i]);
+		if (index)
+		{
+			return (i);
+		}
 	}
+	return (-1);
 }
