f0120c48cae7efcf021c2e407bbfd1c69badb144
Cecil Wiafe <cecilwiafe@gmail.com>
2023-04-04 16:46:44 -0700

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * size_t print_listint - prints integers in list
+ * @h: of structure type
+ * Return: return number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
