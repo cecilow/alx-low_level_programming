082d512b11cd5743b5f5de843d9cd402bfaabbcb
Cecil Wiafe <cecilwiafe@gmail.com>
2023-04-04 17:56:03 -0700

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,8 +13,8 @@ void free_listint2(listint_t **head)
 	while (*head != NULL)
 	{
 		temp = *head;
-		free(*head);
-		*head = temp->next;
+		(*head) = (*head)->next;
+		free(temp);
 	}
 	*head = NULL;
 }
